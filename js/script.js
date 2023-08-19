fa481a52a0fb9ee895e9258e03889d6d91a21200
Rizky Saputra <muhamadrizkysaputra34@gmail.com>
2023-08-20 03:42:59 +0700

    Menambahkan file

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toggle class active
+const navbarNav = document.querySelector("nav");
+// ketika hamburger menu di klik
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar side bar untuk menghilangkan nav
+const hamburger = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
